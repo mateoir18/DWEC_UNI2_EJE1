552dd8009f949df98f57fb1b94ce41c542d454c7
mateoir18 <mateoir18@educastur.es>
2023-09-29 14:27:43 +0200

    Sin terminar

---
+++ js/ejercicio1.js
@@ -0,0 +1,9 @@
+var cadena = prompt("Introduce el nombre y los apellidos")
+var sinEspacios = cadena.replaceAll(' ', '');
+
+console.log("La longitud de la cadena es " + sinEspacios.length);
+console.log("cadena en minusculas : " + cadena.toLowerCase());
+console.log("cadena en mayúsculas: " + cadena.toUpperCase)
+
+
+
