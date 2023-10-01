2d7868435fe354698fc9036aed536bb79920962e
mateoir18 <mateoir18@educastur.es>
2023-10-01 21:18:38 +0200

    Ejercicio terminado

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,9 +1,34 @@
 var cadena = prompt("Introduce el nombre y los apellidos")
 var sinEspacios = cadena.replaceAll(' ', '');
+var partes = cadena.split(' ');
+var usuario1 = partes[0] + partes[1].charAt(0) + partes[2].charAt(0);
+var usuario2 = partes[0].charAt(0) + partes[0].charAt(1) + partes[1].charAt(0) + partes[1].charAt(1) + partes[2].charAt(0) + partes[2].charAt(1);
 
-console.log("La longitud de la cadena es " + sinEspacios.length);
-console.log("cadena en minusculas : " + cadena.toLowerCase());
-console.log("cadena en mayúsculas: " + cadena.toUpperCase)
+function generarPasswd(nombre) {
+    var contraseña = "";
+
+    for (var i = 0; i < nombre.length; i++) {
+        contraseña += nombre[i] + i;
+    }
+
+    return contraseña;
+}
+
+
+var nombre = prompt("Introduce tu nombre");
+
+
+
+alert("La longitud de la cadena es " + sinEspacios.length);
+alert("cadena en minusculas : " + cadena.toLowerCase());
+alert("cadena en mayúsculas: " + cadena.toUpperCase);
+alert( "Nombre: " + partes[0] + "\nApellido 1: " + partes[1] + "\nApellido 2: " + partes[2]);
+alert("Propuesta de nombre de usuario 1: " + usuario1);
+alert("Propuesta de nombre de usuario 2: " + usuario2);
+//      RETO EXTRA    //
+alert("Contraseña generada: " + generarPasswd(nombre));
+
+
 
 
 
